ed793b841260f0ac0b0192407eae9af4a8c136f3
ceruleancee <ceruleancee@gmail.com>
2019-03-26 20:24:02 -0500

    Add PoliceOffice

---
+++ src/IPerson.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Application
+{
+    public class IPerson
+    {
+        public IPerson()
+        {
+        }
+    }
+}
---
+++ src/SimpleExample.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ch1_hello_world {
+    class MainClass {
+
+        public static void Main(String[] args){
+            string hello = "Hello World it's Cerulean Cee!";
+            DateTime now = DateTime.Now;
+            Console.Write(hello);
+            Console.WriteLine(" The date is " + now.ToLongDateString());
+        }
+    }
+}
