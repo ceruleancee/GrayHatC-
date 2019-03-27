5ada26ced018872cf6a0d47b2f7bd7de7c3c2086
ceruleancee <ceruleancee@gmail.com>
2019-03-26 20:35:22 -0500

    Update Main class

--- src/Main.cs
+++ src/Main.cs
@@ -1,41 +1,35 @@
 using System;
 
-namespace penTest
-{
-    public class MainClass
-    {
-        public static void Main(string[] args)
-        {
-			Firefighter firefighter = new Firefighter("Joe Carrington", 35);
-			firefighter.PensionAmount = 5000;
+namespace penTest {
 
-			PrintNameAndAge(firefighter);
-			PrintPensionAmount(firefighter);
+    public class MainClass{
 
-			firefighter.DriveToPlaceOfInterest();
-
-			PoliceOfficer officer = new PoliceOfficer("Jane Hope", 32);
-			officer.PensionAmount = 5500;
-			officer.HasEmergency = true;
-
-			PrintNameAndAge(officer);
-			PrintPensionAmount(officer);
-
-			officer.DriveToPlaceOfInterest();
+        public static void Main(string[] args){
+          Firefighter firefighter = new Firefighter("Joe Carrington", 35);
+          firefighter.PensionAmount = 5000;
+          PrintNameAndAge(firefighter);
+	        PrintPensionAmount(firefighter);
+          firefighter.DriveToPlaceOfInterest();
+          PoliceOfficer officer = new PoliceOfficer("Jane Hope", 32);
+          officer.PensionAmount = 5500;
+          officer.HasEmergency = true;
+          PrintNameAndAge(officer);
+          PrintPensionAmount(officer);
+          officer.DriveToPlaceOfInterest();
         }
 
-		static void PrintNameAndAge(IPerson person)
-		{
-			Console.WriteLine("Name: " + person.Name);
+		static void PrintNameAndAge(IPerson person){
+      Console.WriteLine("Name: " + person.Name);
 			Console.WriteLine("Age: " + person.Age);
 		}
 
-		static void PrintPensionAmount(PublicServant servant)
-		{
-			if (servant is Firefighter)
-				Console.WriteLine("Pension of firefighter: " + servant.PensionAmount);
-			else if (servant is PoliceOfficer)
-				Console.WriteLine("Pension of officer: " + servant.PensionAmount);
-		}
+		static void PrintPensionAmount(PublicServant servant){
+      if (servant is Firefighter){
+        Console.WriteLine("Pension of firefighter: " + servant.PensionAmount);
+      }
+      else if (servant is PoliceOfficer){
+        Console.WriteLine("Pension of officer: " + servant.PensionAmount);
+      }
     }
+  }
 }
